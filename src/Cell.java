14649d6964fcdea61aca22bb87422cc5fbc99cfa
mattpozzo <mpozzo@estudiantes.unsam.edu.ar>
2023-10-25 16:35:01 -0300

    Cell

--- src/Cell.java
+++ src/Cell.java
@@ -1,5 +1,34 @@
 package src;
 
-public class Cell {
-    
+public class Cell<T> {
+    private T value;
+
+    public Cell(T value) {
+        if (value instanceof Number || value instanceof String || value instanceof Boolean) {
+            this.value = value;
+        } else {
+            throw new IllegalArgumentException("Invalid type for Cell: " + value.getClass().getName());
+        }
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+
+    public void setValue(T newValue) {
+        if (newValue instanceof Number || newValue instanceof String || newValue instanceof Boolean) {
+            this.value = newValue;
+        } else {
+            throw new IllegalArgumentException("Invalid type for Cell: " + value.getClass().getName());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.value.toString();
+    }
+
+    public boolean isNull() {
+        return this.value == null;
+    }
 }
