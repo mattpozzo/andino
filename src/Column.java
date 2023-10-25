a0d77a65b4f40a560d89001b07c896ca3c6b221a
Joaquin Saposnik <joaquin.saposnik@gmail.com>
2023-10-25 18:08:00 -0300

    Clase Columna casi terminada, falta revisar si realmente necesitamos una clase Cell.

--- src/Column.java
+++ src/Column.java
@@ -1,5 +1,27 @@
 package src;
+import java.util.ArrayList;
 
-public class Column {
-    
+
+public class Column<T> {
+    private ArrayList<Cell<T>> cells;
+    private String tag;
+
+    public Column(T[] cellsElements, String tag) {
+        for (int i=0; i < cellsElements.length; i++) {
+            this.cells.add(new Cell<T>(cellsElements[i]));
+        }
+        this.tag = tag;
+    }
+
+    public T getCellValue(int index) {
+        return this.cells.get(index).getValue();
+    }
+
+    public String getTag() {
+        return this.tag;
+    }
+
+    public void replaceNull() {
+
+    }
 }
