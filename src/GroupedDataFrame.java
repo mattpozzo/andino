de2e395e43c9e1cf4f6cffc913a2b7b443cd5564
mattpozzo <mpozzo@estudiantes.unsam.edu.ar>
2023-11-17 13:47:58 -0300

    GroupedDataFrame initial

--- src/GroupedDataFrame.java
+++ src/GroupedDataFrame.java
@@ -1,5 +1,29 @@
 package src;
 
-public class GroupedDataFrame {
+public class GroupedDataFrame extends DataFrame {
+    private DataFrame df;
+    private String[] columnNames;
+
+    public GroupedDataFrame(DataFrame df, String[] columnNames) {
+        this.df = df;
+        this.columnNames = columnNames;
+    }
+
+    public DataFrame getDf() {
+        return this.df;
+    }
+
+    public String[] getColumnNames() {
+        return this.columnNames;
+    }
+
+    public void setDf(DataFrame df) {
+        this.df = df;
+    }
+
+    public void setColumnNames(String[] columnNames) {
+        this.columnNames = columnNames;
+    }
+
     
 }
